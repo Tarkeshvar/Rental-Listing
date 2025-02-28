15c99a836e6d763c82ca42aabb5366fb0c03ac2b
Tarkeshvar <tarkeshvarmani@gmail.com>
2025-03-01 00:09:59 +0530

    Fixed flash

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async (req, res) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", "Welcome to Wonderlust!");
+      req.flash("success", "Welcome to Infinity!");
       res.redirect("/listings");
     });
   } catch (e) {
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome back to Wonderlust!");
+  req.flash("success", "Welcome back to Infinity!");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
